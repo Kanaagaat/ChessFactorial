695580bbae5a3293ad5835e9134d4bb30613175b
Kanagat <kana@MacBook-Air-Kanagat.local>
2026-05-02 18:01:43 +0500

    fix deploy back on railway front on vercel

--- backend/chess_api/settings.py
+++ backend/chess_api/settings.py
@@ -124,8 +124,8 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://chessfactorial-production.up.railway.app",
+    "https://chess-factorial-production.up.railway.app",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
 ]
